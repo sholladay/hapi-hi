605071ee351c2ebeb9a57d5493800276367530fe
Seth Holladay <me@seth-holladay.com>
2016-12-09 17:00:24 -0500

    Simplify with async await.

--- index.js
+++ index.js
@@ -4,44 +4,41 @@ const alignJson = require('json-align');
 const readPkgUp = require('read-pkg-up');
 
 const register = (server, option, next) => {
-    readPkgUp({ cwd : option.cwd })
-        .catch((err) => {
-            next(err);
-        })
-        .then((found) => {
-            const { name : appName, version : appVersion } = found.pkg;
+    (async () => {
+        const { pkg } = await readPkgUp({ cwd : option.cwd });
+        const { name : appName, version : appVersion } = pkg;
 
-            server.route({
-                method : 'GET',
-                path   : (option.noConflict ? `/__${appName}` : '') + '/status',
-                config : {
-                    tags        : ['health', 'status', 'monitor'],
-                    description : 'Check if the server is healthy.',
-                    auth        : false,
-                    handler(request, reply) {
-                        const status = {
-                            appName,
-                            appVersion,
-                            statusCode : 200,
-                            status     : 'OK',
-                            time       : (new Date()).toISOString(),
-                            process    : {
-                                uptime  : process.uptime(),
-                                title   : process.title,
-                                version : process.versions.node,
-                                pid     : process.pid
-                            }
-                        };
+        server.route({
+            method : 'GET',
+            path   : (option.noConflict ? `/__${appName}` : '') + '/status',
+            config : {
+                tags        : ['health', 'status', 'monitor'],
+                description : 'Check if the server is healthy.',
+                auth        : false,
+                handler(request, reply) {
+                    const status = {
+                        appName,
+                        appVersion,
+                        statusCode : 200,
+                        status     : 'OK',
+                        time       : (new Date()).toISOString(),
+                        process    : {
+                            uptime  : process.uptime(),
+                            title   : process.title,
+                            version : process.versions.node,
+                            pid     : process.pid
+                        }
+                    };
 
-                        reply(alignJson(status))
-                        // Inform Hapi that our string is actually valid JSON.
-                        .type('application/json');
-                    }
+                    reply(alignJson(status))
+                    // Inform Hapi that our string is actually valid JSON.
+                    .type('application/json');
                 }
-            });
-
-            next();
+            }
         });
+
+        next();
+    })().catch(next);
 };
 
 register.attributes = {
